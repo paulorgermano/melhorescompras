2ab174ffac4a1971082b9edd5070b50074f5ae67
Paulo Germano <paulor.germano@outlook.com>
2023-04-22 22:33:36 -0300

    uau! Retornamos a versão com 3 notas, inicialmente estabelecido.

---
+++ calculanotamediaprod_2notas.py
@@ -0,0 +1,19 @@
+titulo = "Calcula o valor médio de avaliação do produto"
+print(f'{titulo:^50}')
+
+nota1 = int(input("Digite a primeira nota de avaliação do produto: "))
+
+nota2 = int(input("Digite a segunda nota de avaliação do produto: "))
+
+media = (float(nota1) + float(nota2))/7
+
+print("A média alcançada foi: %s" % media)
+
+if media > 6:
+    print("Parabéns, produto adequado aos clientes!")
+else:
+    print("Que pena, esse produto inadequado e com muitas reclamações. 
+		   Acione o controle de qualidade!")
+		   
+		   
+		   
